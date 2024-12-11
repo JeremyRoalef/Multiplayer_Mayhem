9b87d37dbdfda78f991fedee0ac201cc406b0b6a
JeremyRoalef <roalef944@morrisville.edu>
2024-12-11 15:21:13 -0500

    3.8 Joining A Relay

--- MultiplayerMayhem/Assets/Networking/Client/ClientGameManager.cs
+++ MultiplayerMayhem/Assets/Networking/Client/ClientGameManager.cs
@@ -1,13 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Unity.Netcode.Transports.UTP;
+using Unity.Netcode;
+using Unity.Networking.Transport.Relay;
 using Unity.Services.Core;
+using Unity.Services.Relay.Models;
+using Unity.Services.Relay;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ClientGameManager
 {
     const string MENUSCENENAME = "Menu";
+    JoinAllocation allocation;
 
     //Async allows you to run the method asynchronous of the code running (Runs in background. Returns when it is done)
     public async Task<bool> InitAsync()
@@ -25,5 +32,23 @@ public class ClientGameManager
     public void GoToMenu()
     {
         SceneManager.LoadScene(MENUSCENENAME);
+    }
+
+    public async Task StartClientAsync(string joinCode)
+    {
+        try
+        {
+            allocation = await Relay.Instance.JoinAllocationAsync(joinCode);
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e);
+            return;
+        }
+
+        UnityTransport transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
+        RelayServerData relayServerData = new RelayServerData(allocation, "dtls");
+        transport.SetRelayServerData(relayServerData);
+        NetworkManager.Singleton.StartClient();
     }
 }
--- MultiplayerMayhem/Assets/Scripts/UI/MainMenu.cs
+++ MultiplayerMayhem/Assets/Scripts/UI/MainMenu.cs
@@ -1,10 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.Netcode;
 using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField]
+    TMP_InputField joinCodeField;
+
+    public async void StartClient()
+    {
+        await ClientSingleton.Instance.GameManager.StartClientAsync(joinCodeField.text);
+    }
+
     public async void StartHost()
     {
         await HostSingleton.Instance.GameManager.StartHostAsync();
