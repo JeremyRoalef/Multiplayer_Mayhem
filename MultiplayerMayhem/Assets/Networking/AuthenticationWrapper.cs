337850c09f82a69e209cca351092e7ad8c3f4d89
JeremyRoalef <roalef944@morrisville.edu>
2024-12-11 14:26:47 -0500

    3.5 Auth Improvements

--- MultiplayerMayhem/Assets/Networking/AuthenticationWrapper.cs
+++ MultiplayerMayhem/Assets/Networking/AuthenticationWrapper.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Unity.Services.Authentication;
+using Unity.Services.Core;
 using UnityEngine;
 
 public static class AuthenticationWrapper
@@ -15,20 +16,70 @@ public static class AuthenticationWrapper
             return AuthState;
         }
 
+        if (AuthState == AuthState.Authenticating)
+        {
+            Debug.LogWarning("Already Authenticating!");
+            await Authenticating();
+            return AuthState;
+        }
+
+        await SignInAnonymouslyAsync(maxTries);
+
+        return AuthState;
+    }
+
+    private static async Task SignInAnonymouslyAsync(int maxTries)
+    {
         int tries = 0;
+
 
         AuthState = AuthState.Authenticating;
         while (AuthState == AuthState.Authenticating && tries < maxTries)
         {
-            await AuthenticationService.Instance.SignInAnonymouslyAsync();
+            Debug.Log("Authenticating User...");
 
-            if (AuthenticationService.Instance.IsSignedIn && AuthenticationService.Instance.IsAuthorized)
+            try
             {
-                AuthState = AuthState.Authenticated;
+                Debug.Log("Signing in anonymously");
+                await AuthenticationService.Instance.SignInAnonymouslyAsync();
+
+                if (AuthenticationService.Instance.IsSignedIn && AuthenticationService.Instance.IsAuthorized)
+                {
+                    Debug.Log("Sign in successful");
+                    AuthState = AuthState.Authenticated;
+                    break;
+                }
+            }
+            //Fail to authenticate
+            catch (AuthenticationException ex)
+            {
+                Debug.LogError(ex);
+                AuthState = AuthState.Error;
+            }
+            //Internet/Server failure
+            catch (RequestFailedException ex)
+            {
+                Debug.LogError(ex);
+                AuthState = AuthState.Error;
             }
 
             tries++;
             await Task.Delay(1000);
+        }
+
+        //No error, but not successfully authenticated
+        if (AuthState != AuthState.Authenticated)
+        {
+            Debug.LogWarning($"Player was not authorized successfully in {tries} attempts!!!");
+            AuthState = AuthState.TimeOut;
+        }
+    }
+
+    static async Task<AuthState> Authenticating()
+    {
+        while (AuthState == AuthState.Authenticating || AuthState == AuthState.NotAuthenticated)
+        {
+            await Task.Delay(200);
         }
 
         return AuthState;
