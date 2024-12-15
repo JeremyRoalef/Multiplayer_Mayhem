53e2de4289c2a09b415ab82370d418bb656f92f2
JeremyRoalef <roalef944@morrisville.edu>
2024-12-15 12:00:13 -0500

    3.15 Networking Improvements

---
+++ MultiplayerMayhem/Assets/Editor/StartupSceneLoader.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using System;
+
+
+//This script always runs and will run automatically.
+[InitializeOnLoad]
+public static class StartupSceneLoader
+{
+    static StartupSceneLoader()
+    {
+        EditorApplication.playModeStateChanged += LoadStartupScene;
+    }
+
+    private static void LoadStartupScene(PlayModeStateChange state)
+    {
+        if (state == PlayModeStateChange.ExitingEditMode)
+        {
+            EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+        }
+
+        if (state == PlayModeStateChange.EnteredPlayMode)
+        {
+            if (EditorSceneManager.GetActiveScene().buildIndex != 0)
+            {
+                EditorSceneManager.LoadScene(0);
+            }
+        }
+    }
+}
